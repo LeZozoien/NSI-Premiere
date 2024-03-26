4f298fb2dc5a08519c36a0b2e9ab873533a20173
Le_Zozoien <rodriguenzo67@gmail.com>
2024-03-26 16:01:26 +0100

    Added 2048 graphics

--- Code perso/2048/main.py
+++ Code perso/2048/main.py
@@ -1,9 +1,25 @@
 from random import randint
+import pygame
 
 cases = [[0, 0, 0, 0],
          [0, 0, 0, 0],
          [0, 0, 0, 0],
          [0, 0, 0, 0],]
+
+COLORS = {
+    0:(0, 0, 0),
+    2:(0, 0, 127),
+    4:(0, 0, 255),
+    8:(0, 127, 0),
+    16:(0, 127, 127),
+    32:(0, 127, 255),
+    64:(0, 255, 0),
+    128:(0, 255, 127),
+    256:(0, 255, 255),
+    512:(255, 0, 0),
+    1024:(255, 255, 0),
+    2048:(255, 255, 255),
+}
 
 begin_case = (randint(0, 3), randint(0, 3))
 
@@ -70,8 +86,54 @@ def add_value(grid):
             grid[case_random[0]][case_random[1]] = 2
     return grid
 
-while not is_full(cases):
+def draw_game(grid):
+    screen.fill((0, 0, 0))
+    for x in range(4):
+        for y in range(4):
+            cellColor = COLORS[grid[y][x]]
+            pygame.draw.rect(screen, cellColor, ((x*cellSize[0], y*cellSize[1]), cellSize))
+
+
+pygame.init()
+
+background_colour = (0, 0, 0)
+windowSize = (400, 400)
+halfSize = (200, 200)
+cellSize = (windowSize[0]//4, windowSize[1]//4)
+
+screen = pygame.display.set_mode(windowSize)
+screen.fill(background_colour)
+
+pygame.display.set_caption('Window')
+pygame.display.flip()
+
+
+running = True
+
+while running:
+    for event in pygame.event.get():  
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                if not move(2, cases) == cases:
+                    cases = move(2, cases)
+                    cases = add_value(cases)
+            if event.key == pygame.K_UP:
+                if not move(0, cases) == cases:
+                    cases = move(0, cases)
+                    cases = add_value(cases)
+            if event.key == pygame.K_LEFT:
+                if not move(3, cases) == cases:
+                    cases = move(3, cases)
+                    cases = add_value(cases)
+            if event.key == pygame.K_RIGHT:
+                if not move(1, cases) == cases:
+                    cases = move(1, cases)
+                    cases = add_value(cases)
+    if is_full(cases): running = False
     show_grid(cases)
-    cases = move(int(input("dir")), cases)
-    cases = add_value(cases)
+    draw_game(cases)
+    pygame.display.flip()
+
 show_grid(cases)
