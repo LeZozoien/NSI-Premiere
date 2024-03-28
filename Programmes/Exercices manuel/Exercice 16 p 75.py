965233f6e350e524f1d79c22870eed2628664086
Le_Zozoien <rodriguenzo67@gmail.com>
2024-03-28 10:32:22 +0100

    Commented 16 p 75

--- Programmes/Exercices manuel/Exercice 16 p 75.py
+++ Programmes/Exercices manuel/Exercice 16 p 75.py
@@ -1,7 +1,31 @@
-def distance(a:tuple, b:tuple): # a.
+def distance(a:tuple, b:tuple)->float|int: # a.
+
+    # Vérification du type de a et b
+    if not isinstance(a, (tuple, list)):
+        raise TypeError
+    if not isinstance(b, (tuple, list)):
+        raise TypeError
+    
+    # Vérification de la longueur de a et b
+    if len(a) != 2:
+        raise ValueError
+    if len(b) != 2:
+        raise ValueError
+    
+    # Vérification du type des valeurs dans b et a
+    if not isinstance(a[0], (int, tuple)) or not isinstance(a[1], (int, tuple)) or not isinstance(b[0], (int, tuple)) or not isinstance(b[1], (int, tuple)):
+        raise TypeError
+
+
+    # Code principal de calculation de la distance
     xa, ya = a
     xb, yb = b
-    distance = ((xa-xb)**2+(ya-yb)**2)**0.5
+    distance = ((xa-xb)**2+(ya-yb)**2)**0.5 # --> sqrt((bx-ax)²+(by-ay)²)
+
+    # Vérification du type de distance
+    if not isinstance(distance, (int, float)):
+        raise Exception
+
     return distance
 
 def get_size(points:tuple[tuple]): # b.
