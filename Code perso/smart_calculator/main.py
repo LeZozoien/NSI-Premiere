e9552c8c92377145349e8e2d41c070bd261a9132
Le_Zozoien <81014777+LeZozoien@users.noreply.github.com>
2024-04-01 22:49:30 +0200

    all

---
+++ Code perso/smart_calculator/main.py
@@ -0,0 +1,11 @@
+def is_last_calc(calculation:tuple):
+    terme_1 = calculation[0]
+    calcul = calculation[1]
+    terme_2 = calculation[2]
+    terme_1, terme_2 = str(terme_1), str(terme_2)
+    calculs = ["+", "-", "*", "**", "/", "%", "//"]
+    if len(calculation) == 3:
+        if calcul in calculs:
+            if terme_1.isnumeric() and terme_2.isnumeric():
+                return True
+    return False
