74d7186a662ba364fbdb9f22dbcf4bf2839d3c8f
Le_Zozoien <81014777+LeZozoien@users.noreply.github.com>
2024-04-03 22:28:10 +0200

    debugged pyrender v2

--- Code perso/pyrender/v2.py
+++ Code perso/pyrender/v2.py
@@ -7,11 +7,6 @@ class Object:
                     (1, -1, 1),(1, -1, -1),
                     (-1, 1, 1),(-1, 1, -1),
                     (-1, -1, 1),(-1, -1, -1)]
-        
-        self.rotated_vertices = [(1, 1, 1),(1, 1, -1),
-                    (1, -1, 1),(1, -1, -1),
-                    (-1, 1, 1),(-1, 1, -1),
-                    (-1, -1, 1),(-1, -1, -1)]
 
         self.EDGES = [(0, 1), (0, 2), (0, 4),
                 (1, 3), (1, 5), (2, 3),
@@ -24,14 +19,12 @@ class Object:
                 (4, 5, 0), (5, 1, 0),
                 (0, 2, 4), (7, 2, 3),]
                 # (1, 0, 5), (0, 4, 5),]
-        
-        self.rotation = [0, 0, 0]
 
     def project_point(self, index, camera, focal):
 
         # Variables : 
         camx, camy, camz = camera
-        pointx, pointy, pointz = self.rotated_vertices[index]
+        pointx, pointy, pointz = self.VERTICES[index]
 
         # Camera to view:
         pointx -= camx
@@ -44,7 +37,7 @@ class Object:
 
         # Calculate projected coords
         xproj = focal*pointy/pointx
-        yproj = focal*pointy/pointx
+        yproj = focal*pointz/pointx
 
         return xproj, yproj
     
@@ -54,7 +47,7 @@ class Object:
         else: return a, b
 
     def calculate_normal(self, index):
-        a, b, c = self.rotated_vertices[self.FACES[index][0]],self.rotated_vertices[self.FACES[index][1]],self.rotated_vertices[self.FACES[index][2]]
+        a, b, c = self.VERTICES[self.FACES[index][0]],self.VERTICES[self.FACES[index][1]],self.VERTICES[self.FACES[index][2]]
 
         u = (b[0]-a[0], b[1]-a[1], b[2]-a[2])
         v = (c[0]-a[0], c[1]-a[1], c[2]-a[2])
@@ -75,40 +68,13 @@ class Object:
             else:return a, b, c
         else:return None
 
-    def rotation_point(self, index, angles):
-        x, y, z = self.VERTICES[index]
-        angle_x, angle_y, angle_z = angles
-
-        # Conversion des angles en radians
-        angle_x = math.radians(angle_x)
-        angle_y = math.radians(angle_y)
-        angle_z = math.radians(angle_z)
-
-        # Rotation autour de l'axe X
-        new_y_x = y * math.cos(angle_x) - z * math.sin(angle_x)
-        new_z_x = y * math.sin(angle_x) + z * math.cos(angle_x)
-
-        # Rotation autour de l'axe Y
-        new_x_y = x * math.cos(angle_y) + z * math.sin(angle_y)
-        new_z_y = -x * math.sin(angle_y) + z * math.cos(angle_y)
-
-        # Rotation autour de l'axe Z
-        new_x_z = x * math.cos(angle_z) - y * math.sin(angle_z)
-        new_y_z = x * math.sin(angle_z) + y * math.cos(angle_z)
-
-        return (new_x_z, new_y_x, new_z_y)
-    
-    def update_rotations(self):
-        for index in range(len(self.VERTICES)):
-            self.rotated_vertices[index] = self.rotation_point(index, self.rotation)
-
 Camerapos = [4, 0, 0]
 focal = 1
 
 cube = Object()
 
-for angle in range(0, 360, 30):
-    cube.rotation = [angle, 0, 0]
-    cube.update_rotations()
-    for face_index in range(len(cube.FACES)):
-        print(face_index, cube.project_face(face_index, Camerapos, focal))
+for vertex_idx in range(len(cube.VERTICES)):
+    print(cube.project_point(vertex_idx, Camerapos, focal))
+
+for face_idx in range(len(cube.FACES)):
+    print(cube.project_face(face_idx, Camerapos, focal))
