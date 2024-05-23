7c80d4d1e4fdb03dddb7af561dae526855b23980
Le_Zozoien <rodriguenzo67@gmail.com>
2024-05-23 11:42:11 +0200

    Added 9 and 12 p 137

---
+++ Templates/importer.py
@@ -0,0 +1,31 @@
+def read_csv(file:str)->list[dict]:
+    '''Renvoit une conversion du fichier csv spécifié en argument vers une liste de dictionnaires
+    Attention : la première ligne du fichier csv doit contenir les titres'''
+    
+    try:
+        with open("livre.csv", "r") as file:
+            db=[]
+            data = file.read()
+            entries = data.split("\n")
+            first_line = entries[0]
+            title = []
+            for item in first_line.split(","):
+                    item = item.strip()
+                    title.append(item)
+            entries = entries[1::]
+            for entry in entries:
+                line_dict = {}
+                line=[]
+                for item in entry.split(","):
+                    item = item.strip()
+                    line.append(item)
+                for index, object in enumerate(title):
+                    line_dict[object] = line[index]
+                db.append(line_dict)
+    except FileNotFoundError:
+        raise FileNotFoundError("Le fichier n'existe pas")
+
+if __name__ == "__main__":
+    print(read_csv("livre.csv"))
+    print(read_csv("abonnes.csv"))
+    print(read_csv("emprunt.csv"))
